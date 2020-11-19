b9ad4aa5489b851c3d3cc3001e4b14ad097fd653
Ekaterina Deynega <deynega.katena@mail.ru>
2020-11-19 17:34:07 +0300

    finish

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,25 @@
-// Copyright 2020 Your Name <your_email>
-
+// Copyright 2018 Your Name <your_email>
+#include "tester.hpp"
 #include <gtest/gtest.h>
 
+
+
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+  EXPECT_TRUE(true);
+}
+
+TEST(Ex, EmptyTest) {
+  int L1 = 96;
+  int L3 = 24576;
+  std::vector<int> series;
+  double start = L1 * 0.5*1024/sizeof(int);
+  double end = L3 * 1.5*1024/sizeof(int);
+  while (start < end){
+    series.push_back(start);
+    start = start*2;
+  }
+  series.push_back(end);
+  Tester tester(series);
+  tester.experiment();
+  EXPECT_TRUE(true);
 }
